f26f2cc5a1ca0e33eedc6a0ce744c973a4dee149
ghostmanish <mgoshwami19@gmail.com>
2024-02-08 14:04:57 +0530

    Added band name generator

---
+++ Day_1/band_name.py
@@ -0,0 +1,7 @@
+print("Welcome To band name generator")
+
+city_name = input("Enter your city Name ")
+
+pet_name = input("Enter your pet Name ")
+
+print(" Your band name could be",city_name+pet_name)
