7899bc13273b76228bad357d0e234bce05ab309e
ghostmanish <mgoshwami19@gmail.com>
2024-03-03 00:24:23 +0530

    variable lecture

---
+++ Day_1/variable.py
@@ -0,0 +1,12 @@
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+
+a,b=b,a
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
