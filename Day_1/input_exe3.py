96f4b3c13aa5d7a3c1752c766759f6c1c23d57b7
ghostmanish <mgoshwami19@gmail.com>
2024-03-03 00:14:39 +0530

    Input function example

---
+++ Day_1/input_exe3.py
@@ -0,0 +1,22 @@
+print(input())
+print(input())
+print(input())
+
+
+#---------------------------------
+
+num1 = int(input())
+num2 = int(input())
+
+print(num1 * num2)
+
+#---------------------------------
+
+numOfLetters = len("Angela")
+print(numOfLetters)
+
+#---------------------------------
+
+name = input()
+
+print(len(name))
