850f8740ee64471c6e6cb3754a2ef32f18fa2485
ghostmanish <mgoshwami19@gmail.com>
2024-03-03 00:51:17 +0530

    Added BMI calculator and adding digit code.

---
+++ Day_2/adding_digit.py
@@ -0,0 +1,8 @@
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+
+add = int(two_digit_number[0]) + int(two_digit_number[1])
+
+print(add)
